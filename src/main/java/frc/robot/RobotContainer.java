8c5090f82555b145ac87c8954cc3519ea3fd3725
Kavinraj Jayaraj <kavinraj.jayaraj@gmail.com>
2023-01-30 21:12:20 -0500

    Problem with Auton Autobuilder constructor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,43 +4,35 @@
 
 package frc.robot;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.event.BooleanEvent;
-import frc.robot.commands.balance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.drive;
 import frc.robot.commands.driveDistance;
 import frc.robot.commands.motor;
+import frc.robot.commands.turnToAngle;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Gyro;
 import frc.robot.subsystems.OperatorInterface;
 import frc.robot.subsystems.Pneumatics;
-import frc.robot.subsystems.drivebase.SwerveWheel;
 import frc.robot.subsystems.drivebase.SwerveWheelController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.Button;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -130,7 +122,28 @@ public class RobotContainer implements Constants {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // Create config for trajectory
+
+    ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup("Auton Test path", new PathConstraints(4, 3));
+
+    HashMap<String, Command> eventMap = new HashMap<>();
+      //eventMap.put("marker1", new PrintCommand("Passed marker 1"));
+      //eventMap.put("intakeDown", turnTOturnToAngle(10, m_controller));
+
+    SwerveAutoBuilder autoBuilder =  new SwerveAutoBuilder(
+      SwerveWheelController::getPose, // Pose2d supplier
+      new Pose2d(0, 0, new Rotation2d(0)), // Pose2d consumer, used to reset odometry at the beginning of auto
+      kDriveKinematics, // SwerveDriveKinematics
+      new PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
+      new PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
+      SwerveWheelController::setState, // Module states consumer used to output to the drive subsystem
+      eventMap,
+      true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+      m_Drive // The drive subsystem. Used to properly set the requirements of path following commands
+    );
+
+Command fullAuto = autoBuilder.fullAuto(pathGroup);
+   
+  /*// Create config for trajectory
     TrajectoryConfig config =
         new TrajectoryConfig(
                 kPhysicalDriveMaxSpeed,
@@ -168,7 +181,7 @@ public class RobotContainer implements Constants {
     // Reset odometry to the starting pose of the trajectory.
     m_controller.resetOdometry(exampleTrajectory.getInitialPose());
 
-    // Run path following command, then stop at the end.
-    return swerveControllerCommand.andThen(() -> m_controller.setSpeed(0, 0, 0, true));
+    // Run path following command, then stop at the end.*/
+    return fullAuto;
   }
 }
