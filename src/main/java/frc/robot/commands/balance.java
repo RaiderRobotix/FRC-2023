6e9fab1d46abecf4913972b778ef25a311d5e349
Zachary <zacharyh728@gmail.com>
2023-03-03 16:05:03 -0500

    added balance command

---
+++ src/main/java/frc/robot/commands/balance.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants;
+import frc.robot.UniqueConstants;
+import frc.robot.subsystems.Swerve;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+
+//Command balances robot on charging pad automatically
+
+public class balance extends PIDCommand implements UniqueConstants {
+  /** Creates a new balance. */
+  public balance(Swerve mSwerve) {
+    super(
+        // The controller that the command will use
+        new PIDController(balanceKp, balanceKi, balanceKd),
+        // This should return the measurement
+        () -> mSwerve.getPitch(),
+        // This should return the setpoint (can also be a constant)
+        () -> 0,
+        // This uses the output
+        output -> mSwerve.drive(new Translation2d(output, 0), 0, true, true));
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+    getController().setTolerance(robotBalanceTolerance);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return getController().atSetpoint();
+  }
+}
